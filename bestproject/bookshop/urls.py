4fc67034e47e7f8913464fd11aaca13411bf1f6e
DianaGrass <57933812+DianaGrass@users.noreply.github.com>
2020-04-08 17:18:30 +0300

    Update urls.py

--- bestproject/bookshop/urls.py
+++ bestproject/bookshop/urls.py
@@ -2,6 +2,8 @@ from django.urls import re_path
 from bookshop import views
 
 urlpatterns = [
-        re_path('^$', views.hello),
-        re_path("^world/$", views.world),
+        re_path(r'^$', views.hello),
+        re_path(r"^world/$", views.world, name="world_page"),
+        re_path(r"^just_url/(?P<id_book>[\d]+)/$",
+                views.comment_handler, name="comment_url"),
 ]
