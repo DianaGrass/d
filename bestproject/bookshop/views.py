87a7abac059d4623ea3f4f451a0b633f1e9cfbfd
DianaGrass <57933812+DianaGrass@users.noreply.github.com>
2020-04-08 17:19:18 +0300

    Update views.py

--- bestproject/bookshop/views.py
+++ bestproject/bookshop/views.py
@@ -1,8 +1,8 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import logging
-
+from bookshop.forms import CommentForm
+from bookshop import models
 
 logger = logging.getLogger('django')
 
@@ -14,9 +14,19 @@ def hello(request):
 
 def world(request):
     response = {}
-    response['name'] = "Egor"
-    response['users'] = ["Pavel", "Eugen", "Hajima", "Gaben"]
+    response['all_books'] = models.Book.objects.all()
+    response['comment'] = CommentForm()
     return render(request, './bookshop/index.html', response)
+
+
+def comment_handler(request, id_book):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.comment_book_id = id_book
+        obj.save()
+        return redirect('world_page')
+    return HttpResponse('error')
 
 
 
